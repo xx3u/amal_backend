d88ec11d2feaf53147420e536dfa026c02057f21
Yuliya Khvan <yuliya.khv@gmail.com>
2021-06-25 13:37:26 +0600

    Исправлен config.js используя данные из .env

---
+++ src/config/config.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+
+module.exports = {
+  "development": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": "database_development",
+    "port": 5432,
+    "host": DB_HOST,
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": "database_test",
+    "port": 5432,
+    "host": DB_HOST,
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": "database_production",
+    "port": 5432,
+    "host": DB_HOST,
+    "dialect": "postgres"
+  }
+}
