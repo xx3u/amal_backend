abc11d2843bd0849a6e394d95d50beee2f783b17
Yuliya Khvan <yuliya.khv@gmail.com>
2021-06-26 18:34:35 +0600

    Добавлен метод на обновление данных студента

--- src/controllers/StudentController.js
+++ src/controllers/StudentController.js
@@ -25,5 +25,32 @@ module.exports = {
     } catch (error) {
       console.log(error);
     }
+  },
+  async updateOne(req, res) {
+    try {
+      const student = await Student.findByPk(req.params.id);
+      if (student) {
+        const updatedStudent = await student.update({
+          firstName: req.body.firstName || student.firstName,
+          lastName: req.body.lastName || student.lastName,
+          middleName: req.body.middleName || student.middleName,
+          grade: req.body.grade || student.grade,
+          language: req.body.language || student.language,
+          school: req.body.school || student.school,
+          parentsContacts: req.body.parentsContacts || student.parentsContacts,
+          stream: req.body.stream || student.stream,
+          address: req.body.address || student.address,
+          telephone: req.body.telephone || student.telephone,
+          email: req.body.email || student.email,
+          createdAt: req.body.createdAt,
+          updatedAt: req.body.updatedAt
+        });
+        res.send(updatedStudent);
+      } else {
+        res.send('Student was not found');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
--- src/routes/StudentRoutes.js
+++ src/routes/StudentRoutes.js
@@ -5,5 +5,6 @@ const StudentController = require('../controllers/StudentController');
 router.get('/', StudentController.getAll);
 router.post('/', StudentController.addNew);
 router.get('/:id', StudentController.getById);
+router.put('/:id', StudentController.updateOne);
 
 module.exports = router;
