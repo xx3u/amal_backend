cb18aeb67fcdbcbc1e1c5078b7728779c6ba0105
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-06 16:31:10 +0600

    контроллер для группы был создан

---
+++ src/controllers/ GroupsController.js
@@ -0,0 +1,48 @@
+const Group = require('../models').Group;
+
+module.exports = {
+  async getAll(req, res) {
+    try {
+      const groups = await Group.findAll();
+      return res.status(200).send(groups);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  },
+  async addNew(req, res) {
+    const newGroup = req.body;
+    try {
+      const createdGroup = await Group.create(newGroup);
+      return res.send(createdGroup);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  },
+  async getById(req, res) {
+    try {
+      const group = await Group.findByPk(req.params.id);
+      if (group) {
+        return res.status(200).send(group);
+      } else {
+        return res.status(400).send({ error: 'Group with this id was not found' });
+      }
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+  async updateOne(req, res) {
+    try {
+      const group = await Group.findByPk(req.params.id);
+      if (student) {
+        const updatedGroup = await group.update({
+          groupName: req.body.groupName || group.groupName,
+        });
+        return res.send(updatedGroup);
+      } else {
+        return res.status(400).send({ error: 'Group with this id was not found' });
+      }
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+};
