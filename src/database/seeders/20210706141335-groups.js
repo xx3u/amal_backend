48d07765990a4fc8d6b7c74995b98c737c66af4b
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-06 21:07:28 +0600

    добавлены колонны для даты создания и обновления группы в seeders groups

--- src/database/seeders/20210706141335-groups.js
+++ src/database/seeders/20210706141335-groups.js
@@ -5,15 +5,23 @@ module.exports = {
     return await queryInterface.bulkInsert('Groups', [
       {
         groupName: 'K1',
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
         groupName: 'R7',
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
         groupName: 'K3',
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
         groupName: 'R4',
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
     ]);
   },
