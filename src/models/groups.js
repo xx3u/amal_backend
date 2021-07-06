7b83f717ff81de94f3b7bf1f81c036394dd1a0ed
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-06 16:23:47 +0600

    модель для группы была создана

---
+++ src/models/groups.js
@@ -0,0 +1,24 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Group extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate() {
+      // define association here
+    }
+  }
+  Group.init(
+    {
+      groupName: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'Group',
+    }
+  );
+  return Group;
+};
