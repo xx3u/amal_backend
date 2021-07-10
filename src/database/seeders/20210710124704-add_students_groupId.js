55454bc18a5f1bceffcf45222938411406f1919f
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-07-10 19:56:51 +0600

    Добавил seed

---
+++ src/database/seeders/20210710124704-add_students_groupId.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) => {
+    return queryInterface.sequelize.transaction((t) => {
+      return Promise.all([
+        queryInterface.bulkDelete('Students', null, { transaction: t }),
+        queryInterface.bulkInsert(
+          'Students',
+          [
+            {
+              firstName: 'John',
+              lastName: 'Doe',
+              grade: 10,
+              language: 'ENG',
+              school: '71',
+              parentsContacts: 'grandpa, +77777777',
+              stream: 'SAT',
+              createdAt: new Date(),
+              updatedAt: new Date(),
+              status: 'Активный',
+              groupId: 1,
+            },
+            {
+              firstName: 'Jane',
+              lastName: 'Doe',
+              grade: 5,
+              language: 'KZ',
+              school: '121',
+              parentsContacts: 'mother, +77017777',
+              stream: 'NISH',
+              createdAt: new Date(),
+              updatedAt: new Date(),
+              status: 'В ожидании',
+              groupId: 2,
+            },
+          ],
+          { transaction: t }
+        ),
+      ]);
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    return await queryInterface.bulkDelete('Students', null);
+  },
+};
