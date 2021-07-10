fb8147e95a458fd9bbb9aa33da5cbbdacb27c487
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-07-10 21:22:54 +0600

    Удалил log

--- src/models/group.js
+++ src/models/group.js
@@ -8,7 +8,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      console.log(models);
       this.hasMany(models.Student);
     }
   }
