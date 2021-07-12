b308175421a2ebd17366a2c73a18f10c50f8b1a1
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-12 11:37:43 +0600

    В модели student модель stream изменен на Stream

--- src/models/student.js
+++ src/models/student.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.stream, { foreignKey: 'streamId' });
+      this.belongsTo(models.Stream, { foreignKey: 'streamId' });
     }
   }
   Student.init(
