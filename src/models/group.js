4080c3b1147be6c316694ede956074dc4fcc907d
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-07-17 22:01:36 +0600

    Добавил правила в модель группы

--- src/models/group.js
+++ src/models/group.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Student, { foreignKey: 'groupId' });
+      this.hasMany(models.Student, { foreignKey: 'groupId', onDelete: 'CASCADE', onUpdate: 'SET NULL' });
     }
   }
   Group.init(
