489877518654b45d7f4b301c49bf84cdd1bdcba4
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-07-20 20:46:01 +0600

    добавлена связь между таблицы студентов и оплат

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const studentRoutes = require('./src/routes/StudentRoutes');
 const groupRoutes = require('./src/routes/GroupRoutes');
 const streamRoutes = require('./src/routes/StreamRoutes');
+const testRoutes = require('./src/routes/TestRoutes');
 
 const port = 8080;
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/students', studentRoutes);
 app.use('/groups', groupRoutes);
 app.use('/streams', streamRoutes);
+app.use('/test', testRoutes);
 
 app.get('*', (req, res) =>
   res.status(200).send({
--- src/models/payment.js
+++ src/models/payment.js
@@ -2,13 +2,8 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Payment extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate() {
-      // define association here
+    static associate(models) {
+      this.belongsTo(models.Student, { foreignKey: 'studentId' });
     }
   }
   Payment.init(
@@ -24,6 +19,12 @@ module.exports = (sequelize, DataTypes) => {
       amount: {
         type: DataTypes.INTEGER,
         allowNull: false,
+      },
+      status: {
+        type: DataTypes.BOOLEAN,
+        get() {
+          return true;
+        },
       },
     },
     {
