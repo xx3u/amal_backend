f763b9752db4ba75c3bfc2ae3f0ad1cedd7dbd4d
Zaur Kubanov <ravencount@gmail.com>
2021-07-25 19:03:11 +0600

    Составлен миддлвэр для валидации и файл схемы студента

---
+++ src/schemas/schemas.js
@@ -0,0 +1,15 @@
+const { required } = require('joi');
+const Joi = require('joi');
+
+const StudentSchema = Joi.object({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  grade: Joi.number().required(),
+  language: Joi.string().required(),
+  parentsContacts: Joi.any().required(),
+  streamId: Joi.number().required(),
+  email: Joi.string().email(),
+  status: Joi.string().valid('Активный', 'В резерве', 'Отчисленный', 'В ожидании').required(),
+});
+
+module.exports = { StudentSchema };
