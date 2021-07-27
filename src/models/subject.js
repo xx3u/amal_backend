682cd6c8cb8bc71361af3c30ecb73d7c568a4b75
Zaur Kubanov <ravencount@gmail.com>
2021-07-27 13:56:53 +0600

    Создан предварительный файл модели предметов

---
+++ src/models/subject.js
@@ -0,0 +1,30 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Subject extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+  }
+  Subject.init(
+    {
+      subjectName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      teacherId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Subject',
+    }
+  );
+
+  return Subject;
+};
