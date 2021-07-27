d3dd52a5d6b818d399dc6fe032890f5904a6b09e
Zaur Kubanov <ravencount@gmail.com>
2021-07-27 19:19:46 +0600

    Поправил валидатор

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -3,11 +3,15 @@ const Joi = require('joi');
 const StudentSchema = Joi.object({
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
+  middleName: Joi.string().allow(null, ''),
   grade: Joi.number().required(),
   language: Joi.string().required(),
+  school: Joi.string().allow(null, ''),
   parentsContacts: Joi.any().required(),
   streamId: Joi.number().required(),
-  email: Joi.string().email(),
+  adress: Joi.string().allow(null, ''),
+  telephone: Joi.string().allow(null, ''),
+  email: Joi.string().email().allow(null, ''),
   status: Joi.string().valid('Активный', 'В резерве', 'Отчисленный', 'В ожидании').required(),
 });
 
