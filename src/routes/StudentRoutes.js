61211b5595de526be0fe3b33661f377b08a8e22b
Zaur Kubanov <ravencount@gmail.com>
2021-07-27 18:24:14 +0600

    Удалены лишние элементы от иной ветки

--- src/routes/StudentRoutes.js
+++ src/routes/StudentRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const StudentController = require('../controllers/StudentController');
-const validationMiddleware = require('../middleware/validationMiddleware');
-const { StudentSchema } = require('../schemas/schemas');
 
 router.get('/', StudentController.getByGroupId, StudentController.getAll);
-router.post('/', validationMiddleware(StudentSchema), StudentController.addNew);
+router.post('/', StudentController.addNew);
 router.get('/:id', StudentController.getById);
 router.put('/:id', StudentController.updateOne);
 
