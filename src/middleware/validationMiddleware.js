cfadcd0ed558218d6700e65991357e94140e58cd
Zaur Kubanov <ravencount@gmail.com>
2021-07-27 17:40:37 +0600

    Удалена неиспользуемая переменная и добавлен статус 400 в миддлвэр

--- src/middleware/validationMiddleware.js
+++ src/middleware/validationMiddleware.js
@@ -1,13 +1,10 @@
-// eslint-disable-next-line no-unused-vars
-const Joi = require('joi');
-
 const validationMiddleware = (schema) => {
   return async (req, res, next) => {
     try {
       await schema.validateAsync(req.body);
       next();
     } catch (err) {
-      res.send(err);
+      res.status(400).send(err);
     }
   };
 };
