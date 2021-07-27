608540092c2b53fce1b089ba12091cfefa6ff8d0
Zaur Kubanov <ravencount@gmail.com>
2021-07-27 15:33:16 +0600

    Рауты подключены к файлу сервера

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const studentRoutes = require('./src/routes/StudentRoutes');
 const groupRoutes = require('./src/routes/GroupRoutes');
 const streamRoutes = require('./src/routes/StreamRoutes');
 const paymentRoutes = require('./src/routes/PaymentRoutes');
+const subjectRoutes = require('./src/routes/SubjectRoutes');
 const { PORT } = require('./src/config/config');
 
 const app = express();
@@ -15,6 +16,7 @@ app.use('/students', studentRoutes);
 app.use('/groups', groupRoutes);
 app.use('/streams', streamRoutes);
 app.use('/payments', paymentRoutes);
+app.use('/subjects', subjectRoutes);
 
 app.get('*', (req, res) =>
   res.status(200).send({
