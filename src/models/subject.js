12cd6bef619b99731cafc7ef88cf7181c48749ba
Дүйсенбаев Нұрлан <d.nurlan.d@gmail.com>
2021-07-30 23:13:29 +0600

    созданы связи между моделями "teacher" и "subject"

--- src/models/subject.js
+++ src/models/subject.js
@@ -2,14 +2,8 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Subject extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    // eslint-disable-next-line no-unused-vars
     static associate(models) {
-      // define association here
+      this.hasMany(models.Teacher, { foreignKey: 'subjectId', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
     }
   }
   Subject.init(
