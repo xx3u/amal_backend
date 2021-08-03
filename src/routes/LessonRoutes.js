7635000fa6d63467a1cfd72e9976a9f4bec55ad0
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-08-03 20:56:30 +0600

    Убрал  middleware

--- src/routes/LessonRoutes.js
+++ src/routes/LessonRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const LessonController = require('../controllers/LessonController');
-const { checkLessonsTime } = require('../middleware/lessonsMiddleware');
 const validationMiddleware = require('../middleware/validationMiddleware');
 const { LessonSchema } = require('../schemas/schemas');
 
-router.post('/', validationMiddleware(LessonSchema), checkLessonsTime, LessonController.addNew);
+router.post('/', validationMiddleware(LessonSchema), LessonController.addNew);
 router.delete('/:id', LessonController.deleteById);
 
 module.exports = router;
