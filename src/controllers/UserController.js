d9ab5c6238ff8dd97102acb82c9a4f8c398d2044
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-04 23:22:29 +0600

    создана функция для регистрации нового пользователя

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,8 +1,16 @@
+const User = require('../models').User;
+
 module.exports = {
   async register(req, res) {
     try {
-      console.log('req.body', req.body);
-      return res.status(200).send('Hello world');
+      const { username, password } = req.body;
+      const alreadyExistsUser = await User.findOne({ where: { username } });
+      if (alreadyExistsUser) {
+        return res.status(500).send('User with this username already exists');
+      }
+      const newUser = await new User({ username, password });
+      await newUser.save();
+      return res.status(200).send('Thanks for registering');
     } catch (error) {
       res.status(400).send(error);
     }
