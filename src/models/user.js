ed6859b7e8d3576769a455863b47db7a56f47ff3
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-04 15:18:17 +0600

    добавлены новые поля в модели юзерс

--- src/models/user.js
+++ src/models/user.js
@@ -12,7 +12,20 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   User.init(
-    {},
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
     {
       sequelize,
       modelName: 'User',
