ddd43d06c6fae872a3f7af131f5791e8397f99d1
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-08-06 13:32:39 +0600

    Поправил миграцию user

--- src/database/migrations/20210723111311-create-user.js
+++ src/database/migrations/20210723111311-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -18,7 +18,7 @@ module.exports = {
       },
     });
   },
-  down: async (queryInterface) => {
-    await queryInterface.dropTable('Users');
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Users');
   },
 };
