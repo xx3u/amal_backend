f157e7c78ef8169c2c40f05fb168520527dde000
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-06 16:34:42 +0600

    добавлен функционал получения список студентов только при логине пользователя

--- src/routes/StudentRoutes.js
+++ src/routes/StudentRoutes.js
@@ -3,8 +3,14 @@ const router = express.Router();
 const StudentController = require('../controllers/StudentController');
 const validationMiddleware = require('../middleware/validationMiddleware');
 const { StudentSchema } = require('../schemas/schemas');
+const passport = require('passport');
 
-router.get('/', StudentController.getByGroupId, StudentController.getAll);
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  StudentController.getByGroupId,
+  StudentController.getAll
+);
 router.post('/', validationMiddleware(StudentSchema), StudentController.addNew);
 router.get('/:id', StudentController.getById);
 router.put('/:id', StudentController.updateOne);
