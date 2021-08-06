2f27dbcd1d43878b6d3342ecb5805518302c8eb7
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-06 14:35:58 +0600

    добавлен роутер для логина

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 
-router.post('/', UserController.register);
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
 
 module.exports = router;
