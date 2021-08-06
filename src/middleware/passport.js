e22e9d9bf4cd666ec37e3a4a7098ccd0e2564cc6
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-06 16:32:41 +0600

    создан файл мидлвейр passport.js

---
+++ src/middleware/passport.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const passportJwt = require('passport-jwt');
+const ExtractJwt = passportJwt.ExtractJwt;
+const StrategyJwt = passportJwt.Strategy;
+const User = require('../models').User;
+
+passport.use(
+  new StrategyJwt(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_KEY,
+    },
+    async function (jwtPayload, done) {
+      try {
+        const user = await User.findOne({ where: { id: jwtPayload.id } });
+        return done(null, user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  )
+);
