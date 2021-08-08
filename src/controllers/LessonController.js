235255126df953722f3154634b6138c4bf4131a9
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-08-08 18:31:57 +0600

    Исправил название переменной

--- src/controllers/LessonController.js
+++ src/controllers/LessonController.js
@@ -4,8 +4,8 @@ module.exports = {
   async addNew(req, res) {
     const newLesson = req.body;
     try {
-      const isTimeAvailable = await checkLessonsTime(Lesson, newLesson);
-      if (isTimeAvailable) return res.status(400).send({ message: 'Selected time is not available' });
+      const isTimeUnavailable = await checkLessonsTime(Lesson, newLesson);
+      if (isTimeUnavailable) return res.status(400).send({ message: 'Selected time is not available' });
 
       const createdLesson = await Lesson.create(newLesson);
       return res.send(createdLesson);
