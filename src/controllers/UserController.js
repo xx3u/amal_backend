090a457c6a8a97515bcaec85a5c23fa1ab3323a4
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-08 16:28:32 +0600

    изменены коды ошибок

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -9,14 +9,14 @@ module.exports = {
       const { username, password } = req.body;
       const alreadyExistsUser = await User.findOne({ where: { username } });
       if (alreadyExistsUser) {
-        return res.status(500).send('User with this username already exists');
+        return res.status(400).send({ error: 'User with this username already exists' });
       }
       const hashedPassword = await getHashedPassword(password);
       const newUser = await User.create({ username, password: hashedPassword });
       await newUser.save();
       return res.status(200).send(newUser);
     } catch (error) {
-      res.status(400).send(error);
+      res.status(500).send(error);
     }
   },
   async login(req, res) {
@@ -25,19 +25,19 @@ module.exports = {
 
       const userWithUsername = await User.findOne({ where: { username } });
       if (!userWithUsername) {
-        return res.status(500).send('Email or password does not match');
+        return res.status(400).send({ error: 'Email or password does not match' });
       }
 
       const vaildPassword = await bcrypt.compare(password, userWithUsername.password);
       if (!vaildPassword) {
-        return res.status(500).send('Email or password does not match');
+        return res.status(400).send({ error: 'Email or password does not match' });
       }
 
       const jwtToken = jwt.sign({ id: userWithUsername.id, email: userWithUsername.email }, process.env.JWT_KEY);
 
       return res.status(200).send(jwtToken);
     } catch (error) {
-      res.status(400).send(error);
+      res.status(500).send(error);
     }
   },
 };
