9dde4d1419695424cd238cf98d97b241ff56fa04
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-08 16:00:03 +0600

    сидерс заполнены данными

--- src/database/seeders/20210726151520-mock-data-users.js
+++ src/database/seeders/20210726151520-mock-data-users.js
@@ -5,8 +5,10 @@ module.exports = {
     await queryInterface.bulkInsert(
       'Users',
       [
-        { id: 1, createdAt: new Date(), updatedAt: new Date() },
-        { id: 2, createdAt: new Date(), updatedAt: new Date() },
+        { id: 1, username: 'teacher1', password: 'teacher1', createdAt: new Date(), updatedAt: new Date() },
+        { id: 2, username: 'teacher2', password: 'teacher2', createdAt: new Date(), updatedAt: new Date() },
+        { id: 3, username: 'admin1', password: 'admin1', createdAt: new Date(), updatedAt: new Date() },
+        { id: 4, username: 'student1', password: 'student1', createdAt: new Date(), updatedAt: new Date() },
       ],
       {}
     );
