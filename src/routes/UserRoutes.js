f61c20cbdbaa5363fa3170b09dc75ea3536f5beb
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-11 22:02:32 +0600

    подключил валидации в роут

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const { RegisterSchema, LoginSchema } = require('../schemas/schemas');
+const validationMiddleware = require('../middleware/validationMiddleware');
 
-router.post('/register', UserController.register);
-router.post('/login', UserController.login);
+router.post('/register', validationMiddleware(RegisterSchema), UserController.register);
+router.post('/login', validationMiddleware(LoginSchema), UserController.login);
 
 module.exports = router;
