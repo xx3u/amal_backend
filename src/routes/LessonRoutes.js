014b070b4caa717fbc573a13c598c081e02776fa
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-11 21:17:20 +0600

    добавил passport ограничение на все роуты lesons

--- src/routes/LessonRoutes.js
+++ src/routes/LessonRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const LessonController = require('../controllers/LessonController');
 const validationMiddleware = require('../middleware/validationMiddleware');
 const { LessonSchema } = require('../schemas/schemas');
+const passAuth = require('../middleware/passport');
 
-router.post('/', validationMiddleware(LessonSchema), LessonController.addNew);
-router.delete('/:id', LessonController.deleteById);
+router.post('/', passAuth, validationMiddleware(LessonSchema), LessonController.addNew);
+router.delete('/:id', passAuth, LessonController.deleteById);
 
 module.exports = router;
