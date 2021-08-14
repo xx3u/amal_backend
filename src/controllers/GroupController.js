c865179530a317bcf7d8dfe53e1fddbe32cf5204
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-08-14 23:06:13 +0600

    Исправил ошибку

--- src/controllers/GroupController.js
+++ src/controllers/GroupController.js
@@ -185,7 +185,7 @@ module.exports = {
           lessons.forEach((lesson) => {
             if (dd.getDay() === lesson.startTime.getDay()) {
               const newStartTime = new Date(dd.setHours(lesson.startTime.getHours(), lesson.startTime.getMinutes()));
-              const newEndTime = new Date(dd.setHours(lesson.startTime.getHours(), lesson.startTime.getMinutes()));
+              const newEndTime = new Date(dd.setHours(lesson.endTime.getHours(), lesson.endTime.getMinutes()));
               const newLesson = {
                 startTime: newStartTime,
                 endTime: newEndTime,
