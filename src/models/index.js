922b1b023c0ab25999464ec690e830e33feab20b
Yuliya Khvan <yuliya.khv@gmail.com>
2021-08-17 19:53:34 +0600

    перевели подключение на database_url

--- src/models/index.js
+++ src/models/index.js
@@ -5,15 +5,10 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/database_config')[env];
+const config = require(__dirname + '/../config/database_config');
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config.DATABASE_URL);
 
 fs.readdirSync(__dirname)
   .filter((file) => {
