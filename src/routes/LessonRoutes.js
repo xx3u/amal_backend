fe3d0f324bddd63e271ac45611be4023dc9af9a5
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 12:21:14 +0600

    добавил в роутах уроки мидлвейр

--- src/routes/LessonRoutes.js
+++ src/routes/LessonRoutes.js
@@ -4,8 +4,9 @@ const LessonController = require('../controllers/LessonController');
 const validationMiddleware = require('../middleware/validationMiddleware');
 const { LessonSchema } = require('../schemas/schemas');
 const auth = require('../middleware/passport');
+const accessByRole = require('../middleware/accessByRole');
 
-router.post('/', auth, validationMiddleware(LessonSchema), LessonController.addNew);
-router.delete('/:id', auth, LessonController.deleteById);
+router.post('/', auth, accessByRole('admin'), validationMiddleware(LessonSchema), LessonController.addNew);
+router.delete('/:id', auth, accessByRole('admin'), LessonController.deleteById);
 
 module.exports = router;
