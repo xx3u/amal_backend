61171038e258a353150ffcf9a7fe6fa7b5cf4a3e
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 12:22:31 +0600

    добавил в роутах оплаты мидлвейр

--- src/routes/PaymentRoutes.js
+++ src/routes/PaymentRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const PaymentController = require('../controllers/PaymentController');
+const accessByRole = require('../middleware/accessByRole');
 const auth = require('../middleware/passport');
 
-router.get('/', auth, PaymentController.getByStudentId, PaymentController.getAll);
-router.post('/', auth, PaymentController.addNew);
-router.get('/:id', auth, PaymentController.getById);
-router.put('/:id', auth, PaymentController.updateOne);
+router.get('/', auth, accessByRole('admin'), PaymentController.getByStudentId, PaymentController.getAll);
+router.post('/', auth, accessByRole('admin'), PaymentController.addNew);
+router.get('/:id', auth, accessByRole('admin'), PaymentController.getById);
+router.put('/:id', auth, accessByRole('admin'), PaymentController.updateOne);
 module.exports = router;
