5fe41bb35e0ef80007e700f8ada8e35d5487b068
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 17:17:29 +0600

    добавил req.user

--- src/middleware/accessByRole.js
+++ src/middleware/accessByRole.js
@@ -1,6 +1,6 @@
 const accessByRole = (role) => {
   return async (req, res, next) => {
-    if (req.user.role.toString() !== role) {
+    if (req.user && req.user.role.toString() !== role) {
       return res.status(403).send('You do not have access to this page');
     }
     next();
