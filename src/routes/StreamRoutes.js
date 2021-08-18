7b2a6caaa9ac57f9c47a45ce531dc6bb69cc9812
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 12:24:00 +0600

    добавил в роутахнаправления мидлвейр

--- src/routes/StreamRoutes.js
+++ src/routes/StreamRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const StreamController = require('../controllers/StreamController');
+const accessByRole = require('../middleware/accessByRole');
 const router = express.Router();
 const auth = require('../middleware/passport');
 
 router.get('/', auth, StreamController.getAll);
-router.post('/', auth, StreamController.addNew);
+router.post('/', auth, accessByRole('admin'), StreamController.addNew);
 router.get('/:id', auth, StreamController.getById);
-router.put('/:id', auth, StreamController.updateOne);
-router.delete('/:id', auth, StreamController.deleteById);
+router.put('/:id', auth, accessByRole('admin'), StreamController.updateOne);
+router.delete('/:id', auth, accessByRole('admin'), StreamController.deleteById);
 
 module.exports = router;
