33de34ec77afbf4fff6e0577b0b98c798d2ea1cc
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 12:30:44 +0600

    добавил в роутахюзер мидлвейр

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const { RegisterSchema, LoginSchema } = require('../schemas/schemas');
 const validationMiddleware = require('../middleware/validationMiddleware');
+const accessByRole = require('../middleware/accessByRole');
 
-router.post('/register', validationMiddleware(RegisterSchema), UserController.register);
+router.post('/register', accessByRole('admin'), validationMiddleware(RegisterSchema), UserController.register);
 router.post('/login', validationMiddleware(LoginSchema), UserController.login);
 
 module.exports = router;
