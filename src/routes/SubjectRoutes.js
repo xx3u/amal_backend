1914d3234d150da31176dfa8cb413a9ffd3236a1
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 12:26:12 +0600

    добавил в роутахпредметов мидлвейр

--- src/routes/SubjectRoutes.js
+++ src/routes/SubjectRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const SubjectController = require('../controllers/SubjectController');
+const accessByRole = require('../middleware/accessByRole');
 const auth = require('../middleware/passport');
 
 router.get('/', auth, SubjectController.getAll);
-router.post('/', auth, SubjectController.addNew);
+router.post('/', auth, accessByRole('role'), SubjectController.addNew);
 router.get('/:id', auth, SubjectController.getById);
-router.put('/:id', auth, SubjectController.updateOne);
-router.delete('/:id', auth, SubjectController.deleteById);
+router.put('/:id', auth, accessByRole('role'), SubjectController.updateOne);
+router.delete('/:id', auth, accessByRole('role'), SubjectController.deleteById);
 module.exports = router;
