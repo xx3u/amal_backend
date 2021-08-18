15b7f95ee43c077a0040709c5f55ad840083b8d2
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-18 12:20:08 +0600

    добавил в роутах группы мидлвейр

--- src/routes/GroupRoutes.js
+++ src/routes/GroupRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const GroupController = require('../controllers/GroupController');
+const accessByRole = require('../middleware/accessByRole');
 const auth = require('../middleware/passport');
 
 router.get('/', auth, GroupController.getAll);
-router.post('/', auth, GroupController.addNew);
+router.post('/', auth, accessByRole('admin'), GroupController.addNew);
 router.get('/:id', auth, GroupController.getById);
 router.get('/:id/lessons', auth, GroupController.getGroupsLessons);
-router.put('/:id', auth, GroupController.updateOne);
-router.delete('/:id', auth, GroupController.deleteById);
-router.put('/:id/add-students', auth, GroupController.addStudents);
-router.post('/:id/lessons/add', auth, GroupController.createGroupLessons);
+router.put('/:id', auth, accessByRole('admin'), GroupController.updateOne);
+router.delete('/:id', auth, accessByRole('admin'), GroupController.deleteById);
+router.put('/:id/add-students', auth, accessByRole('admin'), GroupController.addStudents);
+router.post('/:id/lessons/add', auth, accessByRole('admin'), GroupController.createGroupLessons);
 module.exports = router;
