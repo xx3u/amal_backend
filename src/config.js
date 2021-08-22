e593371ab831caf6582b8b59e9ef412ce1436858
Yuliya <yuliyahvan@yahoo.com>
2021-08-22 18:46:45 +0600

    добавлен JWT_SECRET_KEY в config файл

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ module.exports = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   use_env_variable: 'DATABASE_URL',
   dialect: 'postgres',
 };
