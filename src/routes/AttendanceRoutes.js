9a90936e0c2ca4ed4d4912be5aa3ea0edac30568
Yuliya <yuliyahvan@yahoo.com>
2021-08-23 20:39:29 +0600

    добавлен auth middleware

--- src/routes/AttendanceRoutes.js
+++ src/routes/AttendanceRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const AttendanceController = require('../controllers/AttendanceController');
 const validationMiddleware = require('../middleware/validationMiddleware');
 const { AttendanceSchema } = require('../schemas/schemas');
+const auth = require('../middleware/passport');
 
-router.post('/', validationMiddleware(AttendanceSchema) ,AttendanceController.addNew);
-router.delete('/:id', AttendanceController.deleteById);
+router.post('/', auth, validationMiddleware(AttendanceSchema) ,AttendanceController.addNew);
+router.delete('/:id', auth, AttendanceController.deleteById);
 
 module.exports = router;
