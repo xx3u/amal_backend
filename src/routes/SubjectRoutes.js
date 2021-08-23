a408019dd2cdc01ab47ac3d50783146253bf88d1
Bauyrjan Seidahmet <bauke.10.bs@gmail.com>
2021-08-23 21:20:51 +0600

    исправил аргумент мидлвейра

--- src/routes/SubjectRoutes.js
+++ src/routes/SubjectRoutes.js
@@ -5,8 +5,8 @@ const accessByRole = require('../middleware/accessByRole');
 const auth = require('../middleware/passport');
 
 router.get('/', auth, SubjectController.getAll);
-router.post('/', auth, accessByRole('role'), SubjectController.addNew);
+router.post('/', auth, accessByRole('admin'), SubjectController.addNew);
 router.get('/:id', auth, SubjectController.getById);
-router.put('/:id', auth, accessByRole('role'), SubjectController.updateOne);
-router.delete('/:id', auth, accessByRole('role'), SubjectController.deleteById);
+router.put('/:id', auth, accessByRole('admin'), SubjectController.updateOne);
+router.delete('/:id', auth, accessByRole('admin'), SubjectController.deleteById);
 module.exports = router;
