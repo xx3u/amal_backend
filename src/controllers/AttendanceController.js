956789fc1b2f50b699daa731da1af3f358e5e748
Yuliya <yuliyahvan@yahoo.com>
2021-08-23 20:17:59 +0600

    добавлен контроллер attendance addNew

---
+++ src/controllers/AttendanceController.js
@@ -0,0 +1,34 @@
+const { Op } = require('sequelize');
+const Attendance = require('../models').Attendance;
+const Student = require('../models').Student;
+const Lesson = require('../models').Lesson;
+
+module.exports = {
+  async addNew(req, res) {
+    try {
+      const { studentId, lessonId } = req.body;
+      if (!studentId || !lessonId) return res.status(400).send({ error: 'Invalid request parameters' });
+  
+      const student = await Student.findByPk(studentId);
+      if (!student) return res.status(404).send({ error: 'Student with this id was not found' });
+
+      const lesson = await Lesson.findByPk(lessonId);
+      if (!lesson) return res.status(404).send({ error: 'Lesson with this id was not found' });
+
+      const existedAttendance = await Attendance.findOne({
+        where: {
+          [Op.and]: [
+            { studentId },
+            { lessonId },
+          ],
+        }
+      });
+      if (existedAttendance) return res.status(400).send({ error: 'Attendance already exists' });
+
+      const createdAttendance = await Attendance.create({ studentId, lessonId });
+      return res.send(createdAttendance);
+    } catch (error) {
+      return res.status(500).send(error);
+    }
+  },
+};
