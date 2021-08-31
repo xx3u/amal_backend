7d000f66ca3a106c9c4eabedf6ad3a8332f29b9d
Yuliya <yuliyahvan@yahoo.com>
2021-09-01 01:17:39 +0600

    changed yml file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres@backend-db/postgres',
+  DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   use_env_variable: 'DATABASE_URL',
