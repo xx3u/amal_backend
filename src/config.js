9d9c5aa5142360e530c9fb19d9e8f6f9f6cd1f2c
Yuliya <yuliyahvan@yahoo.com>
2021-09-01 00:42:33 +0600

    updates in config file

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT,
-  DATABASE_URL: process.env.DATABASE_URL,
+  DATABASE_URL: process.env.DATABASE_URL || 'http://localhost:8080',
   NODE_ENV: process.env.NODE_ENV,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   use_env_variable: 'DATABASE_URL',
