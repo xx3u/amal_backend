ff9508464d3c07e0a82ac37d6a36506c7d566cbb
Yuliya <yuliyahvan@yahoo.com>
2021-09-07 20:08:35 +0600

    добавлены тесты

--- test/test.js
+++ test/test.js
@@ -1,8 +1,57 @@
 const request = require('supertest');
 const app = require('../server');
+const Student = require('../src/models').Student;
+
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
 
 describe('GET /', () => {
-  it(`responds with Hello World!`, (done) => {
-    request(app).get('/').expect(`Hello World!`, done);
+  let token;
+  beforeAll((done) => {
+    request(app)
+      .post('/login')
+      .send({
+        username: 'admin',
+        password: 'qwerty123',
+      })
+      .end((err, response) => {
+        token = response.body.token; // save the token!
+        done();
+      });
+  });
+  // token not being sent - should respond with a 401
+  test('It should require authorization', () => {
+    request(app)
+      .get('/students')
+      .expect(401)
+  });
+  // send the token - should respond with a 200
+  test('It responds with 200', () => {
+    request(app)
+      .get('/students')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200)
+  });
+
+  test('Should save student to db', async(done) => {
+    await request(app)
+      .post('/students').send({
+        firstName: 'test',
+        lastName: 'test',
+        grade: 5,
+        language: 'KZ',
+        parentsContacts: 'ok',
+        status: 'В ожидании',
+        stream: 1
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    const student = await Student.findOne({ firstName: 'test' });
+    console.log('student', student);
+    expect(student.firstName).toBeTruthy();
+    done();
   })
 });
