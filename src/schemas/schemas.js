55bb227e835bce694c50488190354c2fc64b9e4b
Тарасенко Николай <tng.tarasenko@gmail.com>
2021-09-10 15:51:01 +0600

    Добавил кастомные сообщения и валидацию телефона

--- src/schemas/schemas.js
+++ src/schemas/schemas.js
@@ -37,65 +37,137 @@ const StudentSchema = Joi.object({
     'number.base': `Неверный формат, телефон не должен содержать буквы`,
   }),
   address: Joi.string().allow(null, ''),
-  telephone: Joi.number().allow(null, '').messages({
-    'number.base': `Неверный формат, телефон не должен содержать буквы`,
-  }),
+  telephone: Joi.string()
+    .allow(null, '')
+    .pattern(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)
+    .messages({
+      'string.pattern.base': `Неверный формат, телефон `,
+    }),
   email: Joi.string().email().allow(null, '').messages({
     'string.email': `Неверный формат, Email должен быть типа abc@abc.com `,
   }),
   status: Joi.string().valid('Активный', 'В резерве', 'Отчисленный', 'В ожидании').required(),
 });
 const LessonSchema = Joi.object({
-  startTime: Joi.date().required(),
-  endTime: Joi.date().required(),
-  groupId: Joi.number().required(),
-  teacherId: Joi.number().required(),
-  subjectId: Joi.number().required(),
+  startTime: Joi.date().required().messages({
+    'date.base': `Неверный формат, даты `,
+    'any.required': `startTime не должно быть пустыми`,
+  }),
+  endTime: Joi.date().required().messages({
+    'date.base': `Неверный формат, даты `,
+    'any.required': `endTime не должно быть пустыми`,
+  }),
+  groupId: Joi.number().required().messages({
+    'number.base': `Неверный формат, groupId`,
+    'any.required': `groupId не должно быть пустыми`,
+  }),
+  teacherId: Joi.number().required().messages({
+    'number.base': `Неверный формат, teacherId`,
+    'any.required': `teacherId не должно быть пустыми`,
+  }),
+  subjectId: Joi.number().required().messages({
+    'number.base': `Неверный формат, groupId`,
+    'any.required': `groupId не должно быть пустыми`,
+  }),
 });
 const RegisterSchema = Joi.object({
   username: Joi.string().required().messages({
-    'string.base': `Имя пользователя должно быть текстом'`,
+    'string.base': `Неверный формат,username`,
     'string.empty': `Имя пользователя не должно быть пустым`,
     'any.required': `Имя пользователя не должно быть пустым`,
   }),
-  password: Joi.string().required().min(8),
-  role: Joi.string().valid('admin', 'teacher', 'student').required(),
-  teacherId: Joi.number().allow(null, ''),
+  password: Joi.string().required().min(8).messages({
+    'string.base': `Неверный формат,password`,
+    'string.empty': `Пароль  не должно быть пустым`,
+    'string.min': `Пароль не должен быть меньше 8 символов`,
+    'any.required': `Пароль  не должно быть пустым`,
+  }),
+  role: Joi.string().valid('admin', 'teacher', 'student').required().messages({
+    'string.base': `Неверный формат, role`,
+    'string.empty': `Роль не должно быть пустым`,
+    'any.required': `Роль не должно быть пустым`,
+  }),
+  teacherId: Joi.number().allow(null, '').messages({
+    'number.base': `Неверный формат, teacherId`,
+  }),
 });
 const LoginSchema = Joi.object({
   username: Joi.string().required().messages({
-    'string.base': `Имя пользователя должно быть текстовым'`,
+    'string.base': `Неверный формат,username`,
     'string.empty': `Имя пользователя не должно быть пустым`,
     'any.required': `Имя пользователя не должно быть пустым`,
   }),
   password: Joi.string().required().messages({
-    'string.base': `Пароль не должен быть пустым`,
+    'string.base': `Неверный формат,password`,
     'string.empty': `Пароль не должен быть пустым`,
     'any.required': `Пароль не должен быть пустым`,
   }),
 });
 const PaymentSchema = Joi.object({
-  studentId: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
-  date: Joi.date().required(),
-  amount: Joi.number().required(),
+  studentId: Joi.alternatives().try(Joi.string(), Joi.number()).required().messages({
+    'alternatives.types': `Неверный формат,studentId`,
+    'any.required': `studentId не должен быть пустым`,
+  }),
+  date: Joi.date().required().messages({
+    'date.base': `Неверный формат, даты `,
+    'any.required': `date не должно быть пустыми`,
+  }),
+  amount: Joi.number().required().messages({
+    'number.base': `Неверный формат, Оплаты`,
+    'any.required': `Оплата не должна быть пустой`,
+  }),
   comment: Joi.string().allow(null, ''),
 });
 const TeacherSchema = Joi.object({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
-  language: Joi.string().required(),
-  telephone: Joi.number().required(),
-  email: Joi.string().email().allow(null, ''),
-  subjectId: Joi.number().required(),
+  firstName: Joi.string().required().messages({
+    'string.base': `Неверный формат,Имя должно быть текстом`,
+    'string.empty': `Имя не должно быть пустым`,
+    'any.required': `Имя не должно быть пустым`,
+  }),
+  lastName: Joi.string().required().messages({
+    'string.base': `Неверный формат,Фамилия должна быть текстом`,
+    'string.empty': `Фамилия не должно быть пустым`,
+    'any.required': `Фамилия не должно быть пустым`,
+  }),
+  language: Joi.string().required().messages({
+    'string.base': `Неверный формат, role`,
+    'string.empty': `Роль не должно быть пустым`,
+    'any.required': `Роль не должно быть пустым`,
+  }),
+  telephone: Joi.string()
+    .pattern(/^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/)
+    .required()
+    .messages({
+      'string.pattern.base': `Неверный формат, телефон `,
+    }),
+  email: Joi.string().email().allow(null, '').messages({
+    'string.email': `Неверный формат, Email должен быть типа abc@abc.com `,
+  }),
+  subjectId: Joi.number().required().messages({
+    'number.base': `Неверный формат, subjectId`,
+    'any.required': `Предмет не должен быть пустым`,
+  }),
 });
 const SubjectSchema = Joi.object({
-  subjectName: Joi.string().required(),
+  subjectName: Joi.string().required().messages({
+    'string.base': `Неверный формат, subjectName`,
+    'string.empty': `Предмет не должен быть пустым`,
+    'any.required': `Предмет не должен быть пустым`,
+  }),
 });
 const StreamSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().required().messages({
+    'string.base': `Неверный формат, streamName`,
+    'string.empty': `Направление не должно быть пустым`,
+    'any.required': `Направление не должно быть пустым`,
+  }),
 });
 const GroupSchema = Joi.object({
-  groupName: Joi.string().required(),
+  groupName: Joi.string().required().messages({
+    'string.base': `Неверный формат, groupName`,
+    'string.empty': `Группа не должна быть пустым`,
+    'any.required': `Группа не должна быть пустым`,
+  }),
 });
 module.exports = {
   StudentSchema,
